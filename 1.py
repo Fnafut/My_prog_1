00aa35e31760de8dc718d97e56e01583d8ede4e1
Артем <118684967+Fnafut@users.noreply.github.com>
2024-11-28 12:52:11 +0000

    python 2232

---
+++ 1.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+x = [1, 5, 5, 1, 1]
+y = [5, 5, 1, 1, 5] 
+
+# Создаем график
+plt.figure()
+plt.plot(x, y, marker='o')
+plt.title("Квадрат")
+plt.xlabel("X-координаты")
+plt.ylabel("Y-координаты")
+plt.axis('equal')  # масштаб
+plt.grid(True)
+plt.xlim(0, 6)  #  оси X
+plt.ylim(0, 6)  #  оси Y
+plt.show()
